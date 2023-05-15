d51609a21caeb833cb5458036a1ccfcffae6636b
Farid Alam <faridalam62@gmail.com>
2023-05-16 00:54:49 +0600

    Add user to database added

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
-const express =require('express')
+const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
+require('dotenv').config()
+
 
 app.use(cors());
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.se8uzie.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      const userList = client.db('simple-crud').collection('users');
+
+      app.post('/users', async(req, res) => {
+          const doc = req.body;
+          const result = await userList.insertOne(doc)
+          res.send(result);
+      })
+
+
+      app.get('/users', async(req, res) => {
+          const cursor = userList.find();
+          const result = await cursor.toArray();
+          res.send(result)
+      })
+
+      app.get('/users/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) }
+          const result = await userList.findOne(query)
+          res.send(result)
+      })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/',(req,res)=>{
     res.send('Server is running..')
